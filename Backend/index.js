6d3f580a972a1ce97b966bbb08199513d78a759e
ncluff003 <ncluff003@zohomail.com>
2023-03-06 10:01:09 -0700

    Adding ReadMe to build the documentation.

--- Backend/index.js
+++ Backend/index.js
@@ -3,11 +3,9 @@
 const Server = require('./Server');
 const ScheduleItRouter = require('./Routes/scheduleItRoutes.js');
 const Routes = require('./Routes/routes.js');
-const Controllers = require('./Controllers');
 
 module.exports = {
   server: Server,
   router: ScheduleItRouter,
-  routes: Routes,
-  controllers: Controllers,
+  scheduleItRoute: Routes.scheduleIt.app,
 };
---
+++ README.md
@@ -0,0 +1,4 @@
+<div style="height: max-content; width: 100%; display: flex; flex-flow: column nowrap; justify-content: flex-start; align-items: center" align="center">
+    <h1 align='center' style="font-size: 4rem; font-weight: bold; color: #6EC6FF;">ScheduleIt</h1>
+
+</div>
